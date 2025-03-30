3573de6c4a2b88bebe6354d041f02c5d08dc4636
Amy <chiameilin666@gmail.com>
2025-03-30 16:19:40 +0800

    test2

--- sendMail.py
+++ sendMail.py
@@ -4,20 +4,20 @@ from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 msg = MIMEMultipart()
-msg["From"] = "leowu102000@gmail.com"
-msg["To"] = "jufengwu102000@gmail.com"
+msg["From"] = "chiameilin666@gmail.com"
+msg["To"] = "chiameilin666@gmail.com"
 msg["Subject"] = Header("Test send mail","utf-8").encode()
 msg_content = MIMEText("this is test")
 msg.attach(msg_content)
 ssl_context = ssl.create_default_context()
 """
 server = smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context)
-server.login("leowu102000@gmail.com","uahy rbwi scfc uzen")
-server.sendmail("leowu102000@gmail.com","jufengwu102000@gmail.com",msg.as_string())
+server.login("chiameilin666@gmail.com","zaou ngde xxjy ssmc")
+server.sendmail("chiameilin666@gmail.com","chiameilin666@gmail.com",msg.as_string())
 server.close()
 """
 with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
-  server.login("leowu102000@gmail.com","uahy rbwi scfc uzen")
-  server.sendmail("leowu102000@gmail.com","jufengwu102000@gmail.com",msg.as_string())
+  server.login("chiameilin666@gmail.com","zaou ngde xxjy ssmc")
+  server.sendmail("chiameilin666@gmail.com","chiameilin666@gmail.com",msg.as_string())
 
 print("success send mail")
